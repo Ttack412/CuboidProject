123eee6b6459be0f564a4f9eb32551d6cbc7bea9
Tyler Tackett <ttackett412@yahoo.com>
2017-09-19 14:14:03 -0400

    Committing the cube main class

--- src/cuboidmain/CuboidMain.java
+++ src/cuboidmain/CuboidMain.java
@@ -14,8 +14,26 @@ public class CuboidMain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    public static void main(String[] args) {  
+        
+        
+        Volume volume = new Volume();
+        Cube<Integer> CubeOne = new Cube(1,2,3);
+        Cube<Double> CubeTwo = new Cube();
+        
+        CubeTwo.setHeight(2.5);
+        CubeTwo.setLength(2.5);
+        CubeTwo.setWidth(2.5);
+        
+        System.out.println(CubeOne);
+        System.out.println(CubeTwo);
+        
+        System.out.println("");
+        
+        System.out.println("Cube One's volume is " + volume.getVolume(CubeOne));
+        
+        System.out.println("");
+        
+        System.out.println("Cube Two's volume is " + volume.getVolume(CubeTwo));
+    } 
 }
